b13d2dde17d7a512daf45465e20ff73ab6e16186
heather otto <heatherotto32@yahoo.com>
2022-10-24 17:57:17 -0500

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace MethodOverloading
+﻿using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MethodOverloading
 {
     public class Program
     {
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool yay)
+        {
+            int answer3 = num1 + num2;
+
+            if (answer3 > 1 && yay == true)
+                return $"{answer3} dollars";
+            
+            else if (answer3 == 1 && yay == true)
+                return $"{answer3} dollar";
+            
+            else
+                return ($"program is stopped.  It is false");
+
+            
+        }
+
+        
+          
+            
+        
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var x = 6;
+            var y = 7;
+            var answerOne = (x + y);
+            
+            Console.WriteLine(answerOne);
+
+            var a = 6.8m;
+            var b = 5.4m;
+            var answerTwo = (a + b);
+
+            Console.WriteLine(answerTwo);
+
+            var answerThree = Add(0, 0, true);
+            Console.WriteLine(answerThree);
+
         }
     }
 }
